e4a9c2cdae15ab645a7994ad23ce2888a5ffd975
HMT <hmt21cen>
2020-12-26 12:51:43 +0330

    import commwent

--- bandaremam/src/main/java/ir/kimiya/bandaremam/BandaremamApplication.java
+++ bandaremam/src/main/java/ir/kimiya/bandaremam/BandaremamApplication.java
@@ -2,7 +2,7 @@ package ir.kimiya.bandaremam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//test
+//test 3
 @SpringBootApplication
 public class BandaremamApplication {
 
